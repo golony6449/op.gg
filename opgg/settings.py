e10433c0a249dbc9633626e039c46186388342f3
golony6449 <ab6449@gmail.com>
2018-11-29 01:26:06 +0900

    ALLOWED_HOST에 127.0.0.1 (localhosts) 추가

--- opgg/settings.py
+++ opgg/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = key_chain['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = [key_chain['ALLOWED_HOSTS']]
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [key_chain['ALLOWED_HOSTS'], '127.0.0.1']
+# ALLOWED_HOSTS = []
 
 # Application definition
 
