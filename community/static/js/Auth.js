1e30579d647183e52612e68a0af3e45cea0c4325
Park Seong Heum <bb0162@live.co.kr>
2018-12-05 23:25:30 +0900

    검색페이지 템플릿 추가

--- community/static/js/Auth.js
+++ community/static/js/Auth.js
@@ -1,3 +1,31 @@
 $(".signup").on("shown.bs.modal", function() {
   $("#signupModal").trigger("focus");
 });
+
+//프로필 업로드
+$(function() {
+  $("#profile-image").click(function() {
+    //이미지 클릭시
+    $(".input-img").click(); //<input type="file" 파일 입력
+    $(".input-img").on("change", handleIgmFileSelect); //변화 발생시 handleImgaFileSelect 함수 실행
+  });
+});
+
+function handleIgmFileSelect(event) {
+  var files = event.target.files;
+  var filesArr = Array.prototype.slice.call(files);
+
+  filesArr.forEach(function(f) {
+    if (!f.type.match("image.*")) {
+      alert("이미지만 사용가능합니다.");
+      return;
+    }
+    sel_file = f;
+
+    var reader = new FileReader();
+    reader.onload = function(event) {
+      $("#profile-image").attr("src", event.target.result);
+    };
+    reader.readAsDataURL(f);
+  });
+}
--- community/static/js/Home.js
+++ community/static/js/Home.js
@@ -2,7 +2,64 @@ var clickCount = 0;
 var sel_file;
 /*글쓰기 버튼*/
 function writePost() {
-  document.write_post.submit();
+  var postContents = $("#input-post").val(); /*포스트 내용*/
+  var username = "@CS.GG"; //글쓴이
+  var writtenTime = "10 min ago"; //글쓴 시간
+  var imgSrc = "https://picsum.photos/50/50"; //프로필 사진
+
+  if (postContents != "") {
+    $(".gedf-card-row").append(
+      `<div class="card gedf-card post-card" id="posts-partition">
+        <div class="card-header">
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="mr-2">
+                <img
+                  class="rounded-circle"
+                  width="45"
+                  src=` +
+        imgSrc +
+        `
+                  alt=""
+                />
+              </div>
+              <div class="ml-2">
+                <div class="h5 m-0">` +
+        username +
+        `</div>
+                <div class="h7 text-muted">Computer Science.Good Game</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card-body">
+          <div class="text-muted h7 mb-2">
+            <i class="fa fa-clock-o"></i>` +
+        writtenTime +
+        `(게시시간)
+          </div>
+        <p class="card-text">` +
+        postContents +
+        `</p>
+        </div>
+        <div class="card-footer">
+          <a href="#" class="card-link">
+          <i class="fa fa-gittip"></i> Like
+          </a>
+        </div>
+      </div>`
+    );
+  }
+  if (
+    $(".post-card:visible").length / 4 > clickCount &&
+    $(".post-card:visible").length / 4 < clickCount + 1
+  ) {
+    $(".gedf-card:hidden").show();
+  }
+  if ($(".gedf-card:hidden").length > 0) {
+    $("#Viewmore").css("display", "block");
+  }
 }
 
 //더보기
@@ -27,13 +84,15 @@ $(function() {
 
 //프로필 업로드
 $(function() {
+  //이미지 클릭시
   $("#profile-image").click(function() {
+    //<input type="file" 파일 입력
     $(".input-img").click();
-    $(".input-img").on("change", handleIgmFileSelect);
+    $(".input-img").on("change", handleImgFileSelect); //변화 발생시 handleImgaFileSelect 함수 실행
   });
 });
 
-function handleIgmFileSelect(event) {
+function handleImgFileSelect(event) {
   var files = event.target.files;
   var filesArr = Array.prototype.slice.call(files);
 
@@ -50,4 +109,16 @@ function handleIgmFileSelect(event) {
     };
     reader.readAsDataURL(f);
   });
+}
+
+function handleFollowButton(event) {
+  if ($(".follow").html() === "Follow") {
+    $(".follow").html("Following");
+    $(".follow").css("background-color", "#2bac85");
+    $(".follow").css("color", "#ffffff");
+  } else {
+    $(".follow").html("Follow");
+    $(".follow").css("background-color", "#ffffff00");
+    $(".follow").css("color", "#2bac85");
+  }
 }
